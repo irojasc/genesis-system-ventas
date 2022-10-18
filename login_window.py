6a412ee5013ed3c8b0d54b52599b475f2c5455e1
IROJASC <irojasc3@gmail.com>
2022-10-18 16:32:19 -0500

    Archivo login_windown con ware y sale_ dialog funcionando bien

--- login_window.py
+++ login_window.py
@@ -206,6 +206,8 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             self.isupdateWare = False
 
     def open_salesWindow(self):
+        self.sales_dialog.init_condition()
+        self.sales_dialog.widget.setCurrentIndex(0)
         self.sales_dialog.widget.show()
 
     def setupUi(self):
@@ -360,10 +362,11 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         k = 0
         while(enable_datetime):
             self.date_label.setText(datetime.now().strftime("%H:%M %d/%m/%Y"))
-            if self.sales_dialog.widget.isVisible():
-                print("ventana de ventas abierto")
+            if self.sales_dialog.isVisible() or self.sales_dialog.ui_dialog_.isVisible() or \
+                    self.sales_dialog.cust_dialog_.isVisible():
+                self.setEnabled(False)
             elif not self.sales_dialog.widget.isVisible():
-                print("ventana de ventas cerrado")
+                self.setEnabled(True)
             if k < 600:
                 time.sleep(0.5)
                 k += 1
